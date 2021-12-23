2cbd9d36cfe7dac43ce799534abf557f7df0af3d
Moritz Angermann <moritz.angermann@gmail.com>
2021-12-23 20:43:37 +0800

    Initial Commit

---
+++ mobile-core-ios/mobile_core_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobile_core_iosApp.swift
+//  mobile-core-ios
+//
+//  Created by Moritz Angermann on 23/12/21.
+//
+
+import SwiftUI
+
+@main
+struct mobile_core_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
